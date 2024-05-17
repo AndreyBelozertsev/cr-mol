5d39350582b4ddcdf610734f8e0dd4723e0ae3c5
AndreyBelozertsev <andrey.belozertsev@gmail.com>
2024-05-17 16:06:56 +0300

    fix:ErrorTemplate

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,10 +3,10 @@
     <section class="py-8 md:py-16">
         <div class="container">
             <div>
-                <h1 class="_title text-primary mb-5 lg:mb-10 text-2xl lg:text-[38px]/[calc(40/38*100%)]">Возникла техническая ошибка</h1>
+                <h1 class="_title text-primary mb-5 lg:mb-10 text-2xl lg:text-[38px]/[calc(40/38*100%)]">Возникла ошибка</h1>
             </div>
             <div class="content">
-                Ошибка 500  
+                Попробуйте выполнить действие снова!
             </div>
         </div>
     </section>
